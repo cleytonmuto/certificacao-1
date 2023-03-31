803335411454f8dcc16a2f809b66c68f972bd3d9
cleytonmuto <cleytonmuto@gmail.com>
2023-03-30 22:09:34 -0300

    feat: adicionados Cadastros

---
+++ view/Login.py
@@ -0,0 +1,31 @@
+import PySimpleGUI as sg
+
+class Login:
+
+    def __init__(self):
+        pass
+
+    def sgLogin(self):
+        sg.theme("SystemDefault1")
+        loginLayout = [
+            [sg.Image(filename="./resources/img/estacio.png")],
+            [sg.Text("Login"),],
+            [sg.Input(key="login")],
+            [sg.Text("Senha")],
+            [sg.Input(key="password", password_char="*", enable_events=True)],
+            [sg.Text("", key="mensagem")],
+            [sg.Button("Login", pad=(20,0), size=(10)), sg.Button("Sair", pad=(20,0), size=(10)), sg.Button("Sobre", pad=(20,0), size=(10))],
+            [sg.Button("Submit", visible=False, bind_return_key=True)]
+        ]
+        loginWindow = sg.Window("Sistema", layout=loginLayout, font="Roboto 14", size=(500,400))
+        while True:
+            event, content = loginWindow.read()
+            if event == "Sair" or event == sg.WIN_CLOSED:
+                sg.Exit()
+                break
+            if event == "Sobre":
+                loginWindow.close()
+        loginWindow.close()
+
+login = Login()
+login.sgLogin()
---
+++ view/TelaPrincipal.py
@@ -0,0 +1,9 @@
+import view.CadastroPerfil as cp
+import view.CadastroSistema as cs
+
+class TelaPrincipal:
+
+    def __init__(self):
+        pass
+
+    
