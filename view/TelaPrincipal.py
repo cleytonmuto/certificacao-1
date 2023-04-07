fbcddcbc992901739e9f3197a05a7fc774b4c731
cleytonmuto <cleytonmuto@gmail.com>
2023-04-07 13:26:57 -0300

    refactor: corrected indentation

--- view/TelaPrincipal.py
+++ view/TelaPrincipal.py
@@ -25,60 +25,60 @@ class App(customtkinter.CTk):
         self.large_test_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "large_test_image.png")), size=(500, 150))
         self.image_icon_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "image_icon_light.png")), size=(20, 20))
         self.home_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "home_dark.png")),
-                                                 dark_image=Image.open(os.path.join(image_path, "home_light.png")), size=(20, 20))
+            dark_image=Image.open(os.path.join(image_path, "home_light.png")), size=(20, 20))
         self.chat_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "chat_dark.png")),
-                                                 dark_image=Image.open(os.path.join(image_path, "chat_light.png")), size=(20, 20))
+            dark_image=Image.open(os.path.join(image_path, "chat_light.png")), size=(20, 20))
         self.add_user_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "add_user_dark.png")),
-                                                     dark_image=Image.open(os.path.join(image_path, "add_user_light.png")), size=(20, 20))
+            dark_image=Image.open(os.path.join(image_path, "add_user_light.png")), size=(20, 20))
 
         self.navigationFrame = customtkinter.CTkFrame(self, corner_radius=0)
         self.navigationFrame.grid(row=0, column=0, sticky="nsew")
 
-        self.navigationFrameLabel = customtkinter.CTkLabel(self.navigationFrame, text="  DEV TEAM 03", image=self.logo_image,
-                                                             compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.navigationFrameLabel = customtkinter.CTkLabel(self.navigationFrame, text="  DEV TEAM 03",
+            image=self.logo_image, compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.navigationFrameLabel.grid(row=0, column=0, padx=20, pady=20)
 
-        self.homeButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40, border_spacing=10, text="Home",
-                                                   fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                   image=self.home_image, anchor="w", command=self.homeButtonEvent)
+        self.homeButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40, border_spacing=10,
+            text="Home", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+            image=self.home_image, anchor="w", command=self.homeButtonEvent)
         self.homeButton.grid(row=1, column=0, sticky="ew")
 
-        self.frameSistemaButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40, border_spacing=10, text="Sistemas",
-                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.chat_image, anchor="w", command=self.frameSistemaButtonEvent)
+        self.frameSistemaButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40,
+            border_spacing=10, text="Sistemas", fg_color="transparent", text_color=("gray10", "gray90"),
+            hover_color=("gray70", "gray30"), image=self.chat_image, anchor="w", command=self.frameSistemaButtonEvent)
         self.frameSistemaButton.grid(row=2, column=0, sticky="ew")
 
-        self.framePerfilButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40, border_spacing=10, text="Perfis",
-                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.chat_image, anchor="w", command=self.framePerfilButtonEvent)
+        self.framePerfilButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40,
+            border_spacing=10, text="Perfis", fg_color="transparent", text_color=("gray10", "gray90"),
+            hover_color=("gray70", "gray30"), image=self.chat_image, anchor="w", command=self.framePerfilButtonEvent)
         self.framePerfilButton.grid(row=3, column=0, sticky="ew")
 
-        self.frameMatrizButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40, border_spacing=10, text="Matriz SoD",
-                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.chat_image, anchor="w", command=self.frameMatrizButtonEvent)
+        self.frameMatrizButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40,
+            border_spacing=10, text="Matriz SoD", fg_color="transparent", text_color=("gray10", "gray90"),
+            hover_color=("gray70", "gray30"), image=self.chat_image, anchor="w", command=self.frameMatrizButtonEvent)
         self.frameMatrizButton.grid(row=4, column=0, sticky="ew")
 
-        self.frameUsuarioButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40, border_spacing=10, text="Usuários",
-                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.add_user_image, anchor="w", command=self.frameUsuarioButtonEvent)
+        self.frameUsuarioButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40,
+            border_spacing=10, text="Usuários", fg_color="transparent", text_color=("gray10", "gray90"),
+            hover_color=("gray70", "gray30"), image=self.add_user_image, anchor="w", command=self.frameUsuarioButtonEvent)
         self.frameUsuarioButton.grid(row=5, column=0, sticky="ew")
 
         self.appearanceModeLabel = customtkinter.CTkLabel(self.navigationFrame, text="Theme:", anchor="w")
         self.appearanceModeLabel.grid(row=6, column=0, padx=20, pady=(40, 0), sticky="ns")
-        self.appearanceModeOptioneMenu = customtkinter.CTkOptionMenu(self.navigationFrame, values=["Dark", "Light", "System"],
-                                                             command=self.changeAppearanceModeEvent)
+        self.appearanceModeOptioneMenu = customtkinter.CTkOptionMenu(self.navigationFrame,
+            values=["Dark", "Light", "System"], command=self.changeAppearanceModeEvent)
         self.appearanceModeOptioneMenu.grid(row=7, column=0, padx=20, pady=10, sticky="ns")
 
         self.scalingLabel = customtkinter.CTkLabel(self.navigationFrame, text="UI Scaling:", anchor="w")
         self.scalingLabel.grid(row=8, column=0, padx=20, pady=10, sticky="ns")
-        self.scalingOptionMenu = customtkinter.CTkOptionMenu(self.navigationFrame, values=["80%", "90%", "100%", "110%", "120%"],
-                                                               command=self.changeScalingEvent)
+        self.scalingOptionMenu = customtkinter.CTkOptionMenu(self.navigationFrame,
+            values=["80%", "90%", "100%", "110%", "120%"], command=self.changeScalingEvent)
         self.scalingOptionMenu.grid(row=9, column=0, padx=20, pady=10, sticky="ns")
 
         self.resizableLabel = customtkinter.CTkLabel(self.navigationFrame, text="Window is Resizable:", anchor="w")
         self.resizableLabel.grid(row=10, column=0, padx=20, pady=10, sticky="ns")
-        self.resizableMenu = customtkinter.CTkOptionMenu(self.navigationFrame, values=["True", "False"],
-                                                             command=self.changeResizableEvent)
+        self.resizableMenu = customtkinter.CTkOptionMenu(self.navigationFrame,
+            values=["True", "False"], command=self.changeResizableEvent)
         self.resizableMenu.grid(row=11, column=0, padx=20, pady=10, sticky="ns")
 
         self.homeFrame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
@@ -164,7 +164,7 @@ class App(customtkinter.CTk):
         customtkinter.set_widget_scaling(int(newScaling.replace("%", "")) / 100)
 
     def changeResizableEvent(self, newResizable: str):
-        self.resizable(newResizable, newResizable)
+        self.resizable(newResizable,newResizable)
     
 if __name__ == "__main__":
     app = App()
