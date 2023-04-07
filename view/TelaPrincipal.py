3b144adba434ea64ff201437bb9d760079dfd897
cleytonmuto <cleytonmuto@gmail.com>
2023-04-07 12:09:27 -0300

    fix: adjusted pad and margins

--- view/TelaPrincipal.py
+++ view/TelaPrincipal.py
@@ -3,6 +3,9 @@ import customtkinter
 import os
 from PIL import Image
 
+import TelaSistema
+import TelaPerfil
+
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
@@ -28,134 +31,134 @@ class App(customtkinter.CTk):
         self.add_user_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "add_user_dark.png")),
                                                      dark_image=Image.open(os.path.join(image_path, "add_user_light.png")), size=(20, 20))
 
-        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
-        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
-        self.navigation_frame.grid_rowconfigure(6, weight=1)
+        self.navigationFrame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.navigationFrame.grid(row=0, column=0, sticky="nsew")
 
-        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  DEV TEAM 03", image=self.logo_image,
+        self.navigationFrameLabel = customtkinter.CTkLabel(self.navigationFrame, text="  DEV TEAM 03", image=self.logo_image,
                                                              compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
+        self.navigationFrameLabel.grid(row=0, column=0, padx=20, pady=20)
 
-        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Home",
+        self.homeButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40, border_spacing=10, text="Home",
                                                    fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                   image=self.home_image, anchor="w", command=self.home_button_event)
-        self.home_button.grid(row=1, column=0, sticky="ew")
+                                                   image=self.home_image, anchor="w", command=self.homeButtonEvent)
+        self.homeButton.grid(row=1, column=0, sticky="ew")
 
-        self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Sistemas",
+        self.frameSistemaButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40, border_spacing=10, text="Sistemas",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.chat_image, anchor="w", command=self.frame_2_button_event)
-        self.frame_2_button.grid(row=2, column=0, sticky="ew")
+                                                      image=self.chat_image, anchor="w", command=self.frameSistemaButtonEvent)
+        self.frameSistemaButton.grid(row=2, column=0, sticky="ew")
 
-        self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Perfis",
+        self.framePerfilButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40, border_spacing=10, text="Perfis",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.chat_image, anchor="w", command=self.frame_3_button_event)
-        self.frame_3_button.grid(row=3, column=0, sticky="ew")
+                                                      image=self.chat_image, anchor="w", command=self.framePerfilButtonEvent)
+        self.framePerfilButton.grid(row=3, column=0, sticky="ew")
 
-        self.frame_4_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Matriz SoD",
+        self.frameMatrizButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40, border_spacing=10, text="Matriz SoD",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.chat_image, anchor="w", command=self.frame_4_button_event)
-        self.frame_4_button.grid(row=4, column=0, sticky="ew")
+                                                      image=self.chat_image, anchor="w", command=self.frameMatrizButtonEvent)
+        self.frameMatrizButton.grid(row=4, column=0, sticky="ew")
 
-        self.frame_5_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Usuários",
+        self.frameUsuarioButton = customtkinter.CTkButton(self.navigationFrame, corner_radius=0, height=40, border_spacing=10, text="Usuários",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.add_user_image, anchor="w", command=self.frame_5_button_event)
-        self.frame_5_button.grid(row=5, column=0, sticky="ew")
+                                                      image=self.add_user_image, anchor="w", command=self.frameUsuarioButtonEvent)
+        self.frameUsuarioButton.grid(row=5, column=0, sticky="ew")
 
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"],
+        self.appearanceModeLabel = customtkinter.CTkLabel(self.navigationFrame, text="Theme:", anchor="w")
+        self.appearanceModeLabel.grid(row=6, column=0, padx=20, pady=(100, 0), sticky="ns")
+        self.appearanceModeOptioneMenu = customtkinter.CTkOptionMenu(self.navigationFrame, values=["Dark", "Light", "System"],
                                                              command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
-        self.scaling_label = customtkinter.CTkLabel(self.navigation_frame, text="UI Scaling:", anchor="w")
-        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["80%", "90%", "100%", "110%", "120%"],
+        self.appearanceModeOptioneMenu.grid(row=7, column=0, padx=20, pady=10, sticky="ns")
+
+        self.scaling_label = customtkinter.CTkLabel(self.navigationFrame, text="UI Scaling:", anchor="w")
+        self.scaling_label.grid(row=8, column=0, padx=20, pady=10, sticky="ns")
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.navigationFrame, values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+        self.scaling_optionemenu.grid(row=9, column=0, padx=20, pady=10, sticky="ns")
 
-        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        self.home_frame.grid_columnconfigure(0, weight=1)
+        self.homeFrame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.homeFrame.grid_columnconfigure(0, weight=1)
 
-        self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame, text="", image=self.large_test_image)
-        self.home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
+        self.homeFrame_large_image_label = customtkinter.CTkLabel(self.homeFrame, text="", image=self.large_test_image)
+        self.homeFrame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
 
-        self.second_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        self.second_frame.grid_columnconfigure(0, weight=1)
-        self.second_frame.grid_rowconfigure(0, weight=1)
+        self.frameSistema = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.frameSistema.grid_columnconfigure(0, weight=1)
+        self.frameSistema.grid_rowconfigure(0, weight=1)
 
-        self.second_frame_label = customtkinter.CTkLabel(self.second_frame, text= "Aba Sistemas",
+        self.telaSistema = TelaSistema.TelaSistema()
+        self.telaSistema.showAt(self.frameSistema)
+
+        self.framePerfil = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.framePerfil.grid_columnconfigure(0, weight=1)
+        self.framePerfil.grid_rowconfigure(0, weight=1)
+
+        self.telaPerfil = TelaPerfil.TelaPerfil()
+        self.telaPerfil.showAt(self.framePerfil)
+
+        self.frameMatriz = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.frameMatriz.grid_columnconfigure(0, weight=1)
+        self.frameMatriz.grid_rowconfigure(0, weight=1)
+
+        self.frameMatriz_label = customtkinter.CTkLabel(self.frameMatriz, text= "Aba da Matriz SoD",
                                                          font=customtkinter.CTkFont(size=48, weight="bold") )
-        self.second_frame_label.grid(row=0,column=0, padx=20, pady=20)
+        self.frameMatriz_label.grid(row=0,column=0, padx=20, pady=20)
 
-        self.third_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        self.third_frame.grid_columnconfigure(0, weight=1)
-        self.third_frame.grid_rowconfigure(0, weight=1)
+        self.frameUsuario = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.frameUsuario.grid_columnconfigure(0, weight=1)
+        self.frameUsuario.grid_rowconfigure(0, weight=1)
 
-        self.third_frame_label = customtkinter.CTkLabel(self.third_frame, text= "Aba Perfis",
+        self.frameUsuarioLabel = customtkinter.CTkLabel(self.frameUsuario, text= "Aba Usuários",
                                                          font=customtkinter.CTkFont(size=48, weight="bold") )
-        self.third_frame_label.grid(row=0,column=0, padx=20, pady=20)
-
-        self.fourth_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        self.fourth_frame.grid_columnconfigure(0, weight=1)
-        self.fourth_frame.grid_rowconfigure(0, weight=1)
-
-        self.fourth_frame_label = customtkinter.CTkLabel(self.fourth_frame, text= "Aba da Matriz SoD",
-                                                         font=customtkinter.CTkFont(size=48, weight="bold") )
-        self.fourth_frame_label.grid(row=0,column=0, padx=20, pady=20)
-
-        self.fifth_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        self.fifth_frame.grid_columnconfigure(0, weight=1)
-        self.fifth_frame.grid_rowconfigure(0, weight=1)
-
-        self.fifth_frame_label = customtkinter.CTkLabel(self.fifth_frame, text= "Aba Usuários",
-                                                         font=customtkinter.CTkFont(size=48, weight="bold") )
-        self.fifth_frame_label.grid(row=0,column=0, padx=20, pady=20)
+        self.frameUsuarioLabel.grid(row=0,column=0, padx=20, pady=20)
     
-    def select_frame_by_name(self, name):
+    def selectFrameByName(self, name):
         # set button color for selected button
-        self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
-        self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
-        self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
-        self.frame_4_button.configure(fg_color=("gray75", "gray25") if name == "frame_4" else "transparent")
-        self.frame_5_button.configure(fg_color=("gray75", "gray25") if name == "frame_5" else "transparent")
+        self.homeButton.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
+        self.frameSistemaButton.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
+        self.framePerfilButton.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
+        self.frameMatrizButton.configure(fg_color=("gray75", "gray25") if name == "frame_4" else "transparent")
+        self.frameUsuarioButton.configure(fg_color=("gray75", "gray25") if name == "frame_5" else "transparent")
 
         # show selected frame
         if name == "home":
-            self.home_frame.grid(row=0, column=1, sticky="nsew")
+            self.homeFrame.grid(row=0, column=1, sticky="nsew")
         else:
-            self.home_frame.grid_forget()
+            self.homeFrame.grid_forget()
 
-        if name == "frame_2":
-            self.second_frame.grid(row=0, column=1, sticky="nsew")
+        if name == "frameSistema":
+            self.frameSistema.grid(row=0, column=1, sticky="nsew")
         else:
-            self.second_frame.grid_forget()
+            self.frameSistema.grid_forget()
 
-        if name == "frame_3":
-            self.third_frame.grid(row=0, column=1, sticky="nsew")
+        if name == "framePerfil":
+            self.framePerfil.grid(row=0, column=1, sticky="nsew")
         else:
-            self.third_frame.grid_forget()
+            self.framePerfil.grid_forget()
         
-        if name == "frame_4":
-            self.fourth_frame.grid(row=0, column=1, sticky="nsew")
+        if name == "frameMatriz":
+            self.frameMatriz.grid(row=0, column=1, sticky="nsew")
         else:
-            self.fourth_frame.grid_forget()
+            self.frameMatriz.grid_forget()
 
-        if name == "frame_5":
-            self.fifth_frame.grid(row=0, column=1, sticky="nsew")
+        if name == "frameUsuario":
+            self.frameUsuario.grid(row=0, column=1, sticky="nsew")
         else:
-            self.fifth_frame.grid_forget()
+            self.frameUsuario.grid_forget()
 
-    def home_button_event(self):
-        self.select_frame_by_name("home")
+    def homeButtonEvent(self):
+        self.selectFrameByName("home")
 
-    def frame_2_button_event(self):
-        self.select_frame_by_name("frame_2")
+    def frameSistemaButtonEvent(self):
+        self.selectFrameByName("frameSistema")
 
-    def frame_3_button_event(self):
-        self.select_frame_by_name("frame_3")
+    def framePerfilButtonEvent(self):
+        self.selectFrameByName("framePerfil")
 
-    def frame_4_button_event(self):
-        self.select_frame_by_name("frame_4")
+    def frameMatrizButtonEvent(self):
+        self.selectFrameByName("frameMatriz")
 
-    def frame_5_button_event(self):
-        self.select_frame_by_name("frame_5")
+    def frameUsuarioButtonEvent(self):
+        self.selectFrameByName("frameUsuario")
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
