3849fbf662f71751e7a00b9af00502df02c95522
cleytonmuto <cleytonmuto@gmail.com>
2023-04-08 17:22:32 -0300

    feat: added hierarchy test

---
+++ resources/img/hierarchyTest.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+grandparent = os.path.dirname(parent)
+sys.path.append(grandparent)
+
+from controller.Controlador import Controlador
+
+obj = Controlador()
+obj.run()
