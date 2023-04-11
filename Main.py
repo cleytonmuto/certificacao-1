2bc977be0dd847c33431df71ee4858632ff0a789
Moisés Eduardo <moises.eduardogc@gmail.com>
2023-04-10 21:23:04 -0300

    Merge branch 'master' of https://github.com/cleytonmuto/certificacao-1

--- Main.py
+++ Main.py
@@ -12,33 +12,34 @@ class Main(customtkinter.CTk):
 
     def __init__(self):
         super().__init__()
-        self.title("Tela de login")
+        self.title("Tela de Login")
+        self.iconbitmap ('view/img/icone.ico')
         self.geometry("300x350+100+100")
-        self.resizable(False, False)
+        self.resizable(False,False)
 
         self.frame = customtkinter.CTkFrame(self)
         self.frame.grid(row=0, column=0, padx=60, pady=20)
 
-        self.label = customtkinter.CTkLabel(self.frame, text="Login", font=("Roboto", 24))
+        self.label = customtkinter.CTkLabel(self.frame, text="Bem Vindo", font=("Roboto", 24))
         self.label.grid(row=1, column=0, padx=10, pady=12)
 
         self.entryLoginVar = ""
-        self.entry1 = customtkinter.CTkEntry(self.frame, placeholder_text="Username",
+        self.entry1 = customtkinter.CTkEntry(self.frame, placeholder_text="Usuário",
             textvariable=self.entryLoginVar)
         self.entry1.grid(row=2, column=0, padx=10, pady=12)
 
         self.entryPasswordVar = ""
-        self.entry2 = customtkinter.CTkEntry(self.frame, placeholder_text="Password",
+        self.entry2 = customtkinter.CTkEntry(self.frame, placeholder_text="Senha",
             textvariable=self.entryPasswordVar,show="*")
         self.entry2.grid(row=3, column=0, padx=10, pady=12)
 
         self.button1 = customtkinter.CTkButton(self.frame, text="Login", command=self.login)
         self.button1.grid(row=4, column=0, padx=10, pady=12)
 
-        self.button2 = customtkinter.CTkButton(self.frame, text="About", command=self.about)
+        self.button2 = customtkinter.CTkButton(self.frame, text="Sobre", command=self.about)
         self.button2.grid(row=5, column=0, padx=10, pady=12)
 
-        self.checkbox = customtkinter.CTkCheckBox(self.frame,text="Remember Me")
+        self.checkbox = customtkinter.CTkCheckBox(self.frame,text="Lembrar de mim")
         self.checkbox.grid(row=6, column=0, padx=10, pady=12)
 
         self.bind("<Return>", self.enterKeyPressed)
